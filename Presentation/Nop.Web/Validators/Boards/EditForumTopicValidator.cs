b06a46f83fd6c798f3c9f2a1f00589faf28559c4
rajibsahani29 <rajibsahani29@gmail.com>
2018-08-22 18:53:52 +0530

    Add files via upload

--- Presentation/Nop.Web/Validators/Boards/EditForumTopicValidator.cs
+++ Presentation/Nop.Web/Validators/Boards/EditForumTopicValidator.cs
@@ -5,10 +5,11 @@ using Nop.Web.Models.Boards;
 
 namespace Nop.Web.Validators.Boards
 {
-    public partial class EditForumPostValidator : BaseNopValidator<EditForumPostModel>
+    public partial class EditForumTopicValidator : BaseNopValidator<EditForumTopicModel>
     {
-        public EditForumPostValidator(ILocalizationService localizationService)
-        {            
+        public EditForumTopicValidator(ILocalizationService localizationService)
+        {
+            RuleFor(x => x.Subject).NotEmpty().WithMessage(localizationService.GetResource("Forum.TopicSubjectCannotBeEmpty"));
             RuleFor(x => x.Text).NotEmpty().WithMessage(localizationService.GetResource("Forum.TextCannotBeEmpty"));
         }
     }
